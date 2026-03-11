3913011a856e4e56c79b91e43008a09b2ed36ee0
ivo gabriel <ivodegange@gmailcom>
2026-03-11 09:22:12 -0300

    cache 60 segundos vercel

--- src/pages/api/cryptos.ts
+++ src/pages/api/cryptos.ts
@@ -1,9 +1,33 @@
 import type { APIRoute } from "astro"
 import { getLatestCryptos } from "../../lib/coinmarketcap"
+import type { Crypto } from "../../types/crypto"
+
+let cache: Crypto[] | null = null
+let lastFetch = 0
+
+const CACHE_TIME = 60 * 1000 
 
 export const GET: APIRoute = async () => {
 
+  const now = Date.now()
+
+  if (cache && now - lastFetch < CACHE_TIME) {
+
+    return new Response(
+      JSON.stringify(cache),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+
+  }
+
   const cryptos = await getLatestCryptos(50)
+
+  cache = cryptos
+  lastFetch = now
 
   return new Response(
     JSON.stringify(cryptos),
@@ -14,4 +38,5 @@ export const GET: APIRoute = async () => {
       }
     }
   )
+
 }
